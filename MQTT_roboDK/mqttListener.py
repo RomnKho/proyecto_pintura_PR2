8ce716a197e31d81c102ed2c25859fef309ec24c
RomnKho <rkhokho@upv.edu.es>
2026-04-29 13:09:28 +0200

    Añadido el handler del robot

--- MQTT_roboDK/mqttListener.py
+++ MQTT_roboDK/mqttListener.py
@@ -2,13 +2,14 @@
 
 import json
 import paho.mqtt.client as mqtt
-# import RobotController  as rc
+import RobotController  as rc
 
 
-broker      = "mqtt.dsic.upv.es"
+broker_dsic = "mqtt.dsic.upv.es"
+free_broker = "broker.emqx.io"
 port        = 1883
-user        = "giirob"
-passwd      = "UPV2024"
+user_dsic   = "giirob"
+passwd_dsic = "UPV2024"
 topic_sub   = "emqx/ESP32_R/sub"
 topic_pub   = "emqx/ESP32_R/pub"
 
@@ -17,32 +18,30 @@ topic_pub   = "emqx/ESP32_R/pub"
 # Retrieved 2026-04-27, License - CC BY-SA 3.0
 
 class Payload(object):
-    def __init__(self, rgb_hex, tamano, cantidad):
+    def __init__(self, rgb_hex, tamano, cantidad, tipo):
         self.rgb_hex = rgb_hex
         self.tamano = tamano
         self.cantidad = cantidad
+        self.tipo = tipo
 
 def as_payload(dct):
-    return Payload(dct['rgb_hex'], dct['tamano'], dct['cantidad'])
-
+    return Payload(dct['rgb_hex'], dct['tamano'], dct['cantidad'], dct['tipo'])
 # Callback
 def on_message(mqttc, obj, msg):
     payload = msg.payload.decode('utf-8')
     topic = msg.topic
     qos = msg.qos
     desJson = json.loads(payload, object_hook = as_payload)
-    message_to_pub = f"Cod: {desJson.rgb_hex}, Sz: {desJson.tamano}, q: {desJson.cantidad}"
+    message_to_pub = f"Cod: {desJson.rgb_hex}, Sz: {desJson.tamano}, q: {desJson.cantidad}, type: {desJson. tipo}"
     mqttc.publish(topic_pub, message_to_pub)
-    
-    # rc.handle_message(mqttc, topic, payload) # for the movement of the robot
-
+    rc.handle_message(mqttc, topic, payload) # for the movement of the robot
 
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_message = on_message
 
-mqttc.username_pw_set(username=user, password=passwd)
-mqttc.connect(broker, port, 60)
+# mqttc.username_pw_set(username=user, password=passwd)
+mqttc.connect(free_broker, port, 60)
 mqttc.subscribe(topic_sub, 0)
 
 mqttc.publish(topic_pub, "Ready from RoboDK")
