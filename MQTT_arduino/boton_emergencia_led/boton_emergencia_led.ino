342a8bd92341226cb73f65a63a533207b1f9203c
RomnKho <rkhokho@upv.edu.es>
2026-05-06 00:13:38 +0200

    Uploaded wrong file

--- MQTT_arduino/boton_emergencia_led/boton_emergencia_led.ino
+++ MQTT_arduino/boton_emergencia_led/boton_emergencia_led.ino
@@ -9,23 +9,25 @@
 #include <ArduinoJson.h>
 
 /* RGB LED */
-static const uint8_t   rgb_pinout[]  = {27, 26, 32};
-static const uint8_t   SZ_RGB_ARRAY  = sizeof(rgb_pinout) / sizeof(rgb_pinout[0]);
-static const uint32_t  FREQ          = 1000;
-static const uint8_t   RESOLUTION    = 8;
-static const uint8_t   CHANNELS[]    = {0, 1, 2};
+static const gpio_num_t   rgb_pinout[]  = {GPIO_NUM_27, GPIO_NUM_26, GPIO_NUM_32};
+static const uint8_t      SZ_RGB_ARRAY  = sizeof(rgb_pinout) / sizeof(rgb_pinout[0]);
+static const uint32_t     FREQ          = 1000;
+static const uint8_t      RESOLUTION    = 8;
+static const uint8_t      CHANNELS[]    = {0, 1, 2};
 
 static uint8_t         rgb_values[SZ_RGB_ARRAY];
-static const float     rgb_balance[SZ_RGB_ARRAY] = {0.5, 0.9, 1.0};
+
+/* BUTTON */
+static const gpio_num_t   button_pin     = GPIO_NUM_4;
 
 /* CONFIG WIFI */
-static const char *ssid          = "MOVISTAR_D005";
-static const char *password      = "vF2qJsNSrtGRzV8pd4La";
+static const char *ssid          = "*";
+static const char *password      = "*";
 
 /* CONFIG MQTT BROKER */
 static const char   *mqtt_broker   = "broker.emqx.io";
-static const char   *topic_pub     = "emqx/ESP32_R/pub";
-static const char   *topic_sub     = "emqx/ESP32_R/sub";
+static const char   *topic_button  = "emqx/ESP32_R/arduino/button";
+static const char   *topic_led     = "emqx/ESP32_R/roboDK/led";
 static const char   *mqtt_username = "emqx";
 static const char   *mqtt_password = "public";
 static const int    mqtt_port     = 1883;
@@ -35,28 +37,18 @@ static WiFiClient espClient;
 static PubSubClient client(espClient);
 
 /* TASK */
-static const int16_t      RGB_TASK_DELAY        = 50;
-static const int16_t      RGB_TASK_RGB_ON_TIME  = 2000;
-static const int8_t       RGB_TASK_PRIORITY     = 3;
-static const int16_t      RGB_TASK_STACK_SZ     = 2048;
-static TaskHandle_t       rgb_task_handle       = NULL;
+static const int16_t      BUTTON_TASK_DELAY        = 50;
+static const int8_t       BUTTON_TASK_PRIORITY     = 3;
+static const int16_t      BUTTON_TASK_STACK_SZ     = 2048;
+static TaskHandle_t       button_task_handle       = NULL;
 
 /* QUEUE */
 static QueueHandle_t    xCallback_rgb_queue   = NULL;
 static const int8_t     CALLBACK_RGB_QUEUE_SZ = 10;
 
-/* DESERIALIZING JSON DOC */
-static JsonDocument          doc;
-static DeserializationError  error;
-
-static const char            *rgb_str;
-static const char            *sz_str;
-static char                  sz_ch;
-static int8_t                quantity;
-
 /* FUNCTIONS */
 void callback  (char *topic, byte *payload, unsigned int length);
-void rgb_task  (void *pvParameters);
+void button_task  (void *pvParameters);
 
 void setup() 
 {
@@ -66,10 +58,9 @@ void setup()
     ledcAttachChannel(rgb_pinout[i], FREQ, RESOLUTION, CHANNELS[i]);
   }
 
-  for(int i = 0; i < SZ_RGB_ARRAY; i++ )
-  {
-    ledcWrite(rgb_pinout[i], 255);
-  }
+  ledcWrite(rgb_pinout[0], 0); // Empieza rojo
+
+  pinMode(button_pin, INPUT_PULLUP);
 
   Serial.begin(115200);
   delay(1000);
@@ -107,18 +98,16 @@ void setup()
 
   delay(500);
   // "Subscribe" to publisher and suscriptor channels
-  client.publish(topic_pub, "Hi, I'm Roman's ESP32");
-  client.subscribe(topic_sub);
-
-  xCallback_rgb_queue = xQueueCreate(CALLBACK_RGB_QUEUE_SZ, sizeof(long)); 
+  client.publish("emqx/ESP32_R/pub", "Hi, I'm Roman's ESP32");
+  client.subscribe(topic_led);
 
   xTaskCreate(
-    rgb_task,
-    "rgb_task",
-    RGB_TASK_STACK_SZ,
+    button_task,
+    "button_task",
+    BUTTON_TASK_STACK_SZ,
     NULL,
-    RGB_TASK_PRIORITY,
-    &rgb_task_handle
+    BUTTON_TASK_PRIORITY,
+    &button_task_handle
   );
 }
 
@@ -132,65 +121,63 @@ void loop() {
 
 void callback(char *topic, byte *payload, unsigned int length) 
 {
-
+  String msg;
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
   Serial.print("Message:");
 
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
+    msg += (char)payload[i];
   }
 
   Serial.println();
   Serial.println("-----------------------");
 
-  error = deserializeJson(doc, payload, length);
-
-  if(error)
+  if(msg == "GREEN")
   {
-    Serial.print("deseralizedJson() returned ");
-    Serial.println(error.c_str());
-    return;
+    ledcWrite(rgb_pinout[0], 255);
+    ledcWrite(rgb_pinout[1], 0);    // Verde al maximo
+    ledcWrite(rgb_pinout[2], 255);
   }
 
-  rgb_str = doc["rgb_hex"] | "000000"; 
-  sz_str = doc["tamano"] | "N";
-  quantity = doc["cantidad"];
-
-  sz_ch = sz_str[0];
-  long rgb_hex_send = strtol(rgb_str, NULL, 16);
-  xQueueSend(xCallback_rgb_queue, &rgb_hex_send, 0);
-  
-  Serial.printf("rgb: %06x; sz: %c; Q: %d\n", rgb_hex_send, sz_ch, quantity);
-  Serial.println("-----------------------");
-
+  if(msg == "RED")
+  {
+    ledcWrite(rgb_pinout[0], 0);    // Rojo al maximo
+    ledcWrite(rgb_pinout[1], 255);    
+    ledcWrite(rgb_pinout[2], 255);
+  }
 }
 
-void rgb_task(void *pvParameters)
+void button_task(void *pvParameters)
 {
+  static bool stop = true;
+
   for(;;)
   {
-    long rgb_hex_received;
-    xQueueReceive(xCallback_rgb_queue, &rgb_hex_received, portMAX_DELAY); 
+      if((digitalRead(button_pin) == LOW) && (stop == true))
+      {
+        client.publish(topic_button, "STOP");
+        stop = !stop;
 
-    rgb_values[0] = (rgb_hex_received >> 16) & 0xFF;
-    rgb_values[1] = (rgb_hex_received >> 8) & 0xFF;
-    rgb_values[2] = rgb_hex_received & 0xFF;
+        while(digitalRead(button_pin) == LOW)
+        {
+          vTaskDelay(pdMS_TO_TICKS(10));
+        }
+      }
 
-    for(int i = 0; i < SZ_RGB_ARRAY; i++)
-    {
-      ledcWrite(rgb_pinout[i], 255 - (rgb_values[i] * rgb_balance[i])); // rgb de anodo comun => 0 brillo máximo | 255 no brilla
-    }
+      if((digitalRead(button_pin) == LOW) && (stop == false))
+      {
+        client.publish(topic_button, "CONTINUE");
+        stop = !stop;
 
-    delay(RGB_TASK_RGB_ON_TIME / portTICK_PERIOD_MS);
+        while(digitalRead(button_pin) == LOW)
+        {
+          vTaskDelay(pdMS_TO_TICKS(10));
+        }
+      }
 
-    for(int i = 0; i < SZ_RGB_ARRAY; i++ )
-    {
-      ledcWrite(rgb_pinout[i], 255);
-    }
-
-    delay(RGB_TASK_DELAY / portTICK_PERIOD_MS);
-
+      vTaskDelay(pdMS_TO_TICKS(BUTTON_TASK_DELAY));
   }
 }
 
@@ -203,7 +190,7 @@ void reconnect()
     if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) 
     {
       Serial.println("conectado");
-      client.subscribe(topic_sub); 
+      client.subscribe(topic_led); 
     } 
     else 
     {
