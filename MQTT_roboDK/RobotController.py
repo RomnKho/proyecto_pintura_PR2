df2b0d63142386b4f68d1e0bd3a59be95f67b7d1
RomnKho <rkhokho@upv.edu.es>
2026-05-06 22:39:40 +0200

    Added Json messages

--- MQTT_roboDK/RobotController.py
+++ MQTT_roboDK/RobotController.py
@@ -24,15 +24,23 @@ class info_pedido:
 
 # ------- FUNCIONES DESEREALIZACION JSON ----------
 
-class Payload(object):
+class payload_pedido(object):
     def __init__(self, rgb_hex, tamano, cantidad, tipo):
         self.rgb_hex = rgb_hex
         self.tamano = tamano
         self.cantidad = cantidad
         self.tipo = tipo
 
-def as_payload(dct):
-    return Payload(dct['rgb_hex'], dct['tamano'], dct['cantidad'], dct['tipo'])
+class payload_button(object):
+    def __init__(self, sensor, estado):
+        self.sensor = sensor
+        self.estado = estado
+
+def as_payload_pedido(dct):
+    return payload_pedido(dct['rgb_hex'], dct['tamano'], dct['cantidad'], dct['tipo'])
+
+def as_payload_button(dct):
+    return payload_button(dct['sensor'], dct['estado'])
 
 # ------- FUNCION DE HANDLE ------------------------
 
@@ -42,25 +50,31 @@ def handle_message(mqttc, topic, payload):
 
     # Conexión con la app
     if topic == topic_sub:
-        desJson = json.loads(payload, object_hook = as_payload)
-        message_to_pub = f"Cod: {desJson.rgb_hex}, Sz: {desJson.tamano}, q: {desJson.cantidad}, type: {desJson. tipo}"
+        desJson_pedido = json.loads(payload, object_hook = as_payload_pedido)
+        message_to_pub = f"Cod: {desJson_pedido.rgb_hex}, Sz: {desJson_pedido.tamano}, q: {desJson_pedido.cantidad}, type: {desJson_pedido. tipo}"
         mqttc.publish(topic_pub, message_to_pub)
 
         # Meto en la cola la info del pedido
-        if desJson.tipo == "Int":
-            Init.cola_info_pedido_interior.put(info_pedido(desJson.rgb_hex, desJson.tamano, desJson.cantidad))
+        if desJson_pedido.tipo == "Int":
+            Init.cola_info_pedido_interior.put(info_pedido(desJson_pedido.rgb_hex, desJson_pedido.tamano, desJson_pedido.cantidad))
 
         else:
-            Init.cola_info_pedido_exterior.put(info_pedido(desJson.rgb_hex, desJson.tamano, desJson.cantidad))
+            Init.cola_info_pedido_exterior.put(info_pedido(desJson_pedido.rgb_hex, desJson_pedido.tamano, desJson_pedido.cantidad))
 
     # Conexión con la ESP32
     if topic == topic_button:
+        desJson_button =  json.loads(payload, object_hook = as_payload_button)
+        message_to_pub = f"sensor: {desJson_button.sensor}, estado: {desJson_button.estado}"
+        mqttc.publish(topic_pub, message_to_pub)
+
         # Si se ha pulsado el boton se hace la parada de emergencia
-        if payload == "STOP":
-            mqttc.publish(topic_led, "GREEN")
+        if desJson_button.estado == "STOP":
+            msg_led = json.dumps({"actuador":"LED","color":"GREEN"})
+            mqttc.publish(topic_led, msg_led)
             RDK.setSimulationSpeed(0)
 
         # Si se vuelve a pulsar el proceso puede continuar
         else:
-            mqttc.publish(topic_led, "RED")
+            msg_led = json.dumps({"actuador":"LED","color":"RED"})
+            mqttc.publish(topic_led, msg_led)
             RDK.setSimulationSpeed(1)
